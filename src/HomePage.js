0002e5355763c2aca74994896f58e22f072fe612
Derek Hudgins <hudgins.derek@gmail.com>
2021-06-30 13:48:36 -0700

    created basic file tree

---
+++ src/HomePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                welcome to the front page of the internet
+            </div>
+        )
+    }
+}
