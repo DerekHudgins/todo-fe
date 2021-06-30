a77069d754dd13e84717267931c2a7963ff1aa7d
Derek Hudgins <hudgins.derek@gmail.com>
2021-06-30 15:39:35 -0700

    login working

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
+import { login } from './Fetch-Utils';
 
 export default class LoginPage extends Component {
+    state = {
+        email: '',
+        password: '',
+    }
+
+    handleSubmit = async e => {
+        e.preventDefault();
+        const token = await login(this.state.email, this.state.password);
+
+        this.props.login(token)
+        this.props.history.push('/todos')
+    }
+
+    handleEmailChange = e => {
+        this.setState({ email: e.target.value });
+    }
+
+    handlePasswordChange = e => {
+        this.setState({ password: e.target.value });
+    }
+
     render() {
-        return (
+        return (   
             <div>
-                
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Email
+                        <input type='email' onChange={this.handleEmailChange}/>
+                    </label>
+                    <label>
+                        password
+                        <input type='password' onChange={this.handlePasswordChange}/>
+                    </label>
+                    <button>Log in</button>
+                </form>
             </div>
         )
     }
